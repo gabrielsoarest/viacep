8baefa3fa3a13b0d6a387170819f7c8a7147af31
gabrielsoarest <g.tavares1993@gmail.com>
2021-07-13 22:00:06 -0300

    Ajuste Classe Endereco

--- azul-cep/src/main/java/com/model/Endereco.java
+++ azul-cep/src/main/java/com/model/Endereco.java
@@ -33,54 +33,29 @@ public class Endereco implements Serializable {
 	public void setCep(String cep) {
 		this.cep = cep;
 	}
-
-
-
 	public String getLogradouro() {
 		return logradouro;
 	}
-
-
-
 	public void setLogradouro(String logradouro) {
 		this.logradouro = logradouro;
 	}
 
-
-
-	
-
-
-
 	public String getBairro() {
 		return bairro;
 	}
-
-
-
 	public void setBairro(String bairro) {
 		this.bairro = bairro;
 	}
-
-
-
 	public String getLocalidade() {
 		return localidade;
 	}
-
-
-
 	public void setLocalidade(String localidade) {
 		this.localidade = localidade;
 	}
 
-
-
 	public String getUf() {
 		return uf;
 	}
-
-
 
 	public void setUf(String uf) {
 		this.uf = uf;
